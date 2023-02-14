72e5b7ba80cb97ad87584fe7f4a0bdb672d5ddca
aleqsio <mikucki@gmail.com>
2023-02-14 18:47:32 +0100

    fix methods

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,16 @@ import {
 } from "./ExpoAudioMixer.types";
 import ExpoAudioMixerModule from "./ExpoAudioMixerModule";
 
-export function play(uri: string, uri2: string): string {
-  return ExpoAudioMixerModule.play(uri, uri2);
+export function play(uri: string, uri2: string) {
+  ExpoAudioMixerModule.play(uri, uri2);
 }
 
-export function setVolume(
-  firstTrackVolume: number,
-  secondTrackVolume: number
-): string {
-  return ExpoAudioMixerModule.setVolume(firstTrackVolume, secondTrackVolume);
+export function setVolume(firstTrackVolume: number, secondTrackVolume: number) {
+  ExpoAudioMixerModule.setVolume(firstTrackVolume, secondTrackVolume);
 }
 
-export function setPan(firstTrackPan: number, secondTrackPan: number): string {
-  return ExpoAudioMixerModule.setVolume(firstTrackPan, secondTrackPan);
+export function setPan(firstTrackPan: number, secondTrackPan: number) {
+  ExpoAudioMixerModule.setVolume(firstTrackPan, secondTrackPan);
 }
 
 export function pause() {
